bd21f672ea5a0b154253f57bff62ee8eddcb5f3a
olowololaye <akinwoleoluwaseun18@gmail.com>
2023-09-14 21:55:58 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#include <stddef.h>
-#include <stdlib.h>
-
-void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
 
